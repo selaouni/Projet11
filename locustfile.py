803da94d016eed78d8ba2dfa0fb086d6fcaa9261
sabah <sabah.elaouni@gmail.com>
2022-12-20 21:48:26 +0100

    locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,35 +1,5 @@
 from locust import HttpUser, TaskSet, task, between
 
-# class UserBehaviour(TaskSet):
-#     @task
-#     def index(self):
-#         self.client.get("/")
-#
-#     def on_stop(self):
-#         self.client.get('/logout')
-#
-#     @task(10)
-#     def index(self):
-#         self.client.get("/")
-#
-#     @task(5)
-#     def purchase_places(self):
-#         self.client.get('/purchasePlaces')
-#
-#     @task(5)
-#     def points(self):
-#         self.client.get("/points_dashboard")
-#
-#     @task(10)
-#     def account(self):
-#         self.client.get('/showSummary')
-#
-#
-# class WebsiteUser(HttpUser):
-#     task_set = UserBehaviour
-#     wait_time = between(5, 9)
-
-
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
@@ -51,5 +21,5 @@ class QuickstartUser(HttpUser):
         self.client.get("/points_dashboard")
 
     @task(10)
-    def account(self):
+    def show_summary(self):
         self.client.get('/showSummary')
