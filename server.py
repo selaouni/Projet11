69144e593201acd5c88c38dbef3e158cfc515a08
sabah <sabah.elaouni@gmail.com>
2022-12-22 12:06:36 +0100

    last version

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def create_app(config):
             return render_template('welcome.html', club=club, competitions=competitions)
 
 
-    @app.route('/purchasePlaces')
+    @app.route('/purchasePlaces',methods=['POST'])
     def purchasePlaces():
         competition = [c for c in competitions if c['name'] == request.form['competition']][0]
         club = [c for c in clubs if c['name'] == request.form['club']][0]
